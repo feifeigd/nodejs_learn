19836d1a353b6899abcf83d61a558752974c6c13
luofei <13632323692@139.com>
2016-10-10 09:55:50 +0800

    test http

---
+++ foo.js
@@ -0,0 +1,6 @@
+
+var _ = require('underscore');
+
+_.each([1,2,3,4,5,6,7,8,9], function(item){
+	console.log('underscore.js says ' + item);
+});
---
+++ server.js
@@ -0,0 +1,10 @@
+
+var http = require('http');
+
+var s = http.createServer(function(req, res){
+	res.writeHead(200, {'Content-Type' : 'text/plain'});
+	res.end("Hello World!\n");
+});
+
+s.listen(3000);
+console.log('Server running at http://127.0.0.1:3000/');
