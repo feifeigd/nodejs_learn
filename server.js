dd1fcbf78ad739785dffba5fcb06489ce3d64710
luofei <13632323692@139.com>
2016-10-10 17:59:37 +0800

    添加路由

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 
-var http = require('http');
+var http = require('http'),
+	url = require('url');
 
 var s = http.createServer(function(req, res){
-	res.writeHead(200, {'Content-Type' : 'text/plain'});
-	res.end("Hello World!\n");
+	var pathname = url.parse(req.url).pathname;
+	if(pathname === '/'){
+		res.writeHead(200, {'Content-Type':'text/plain'});
+		res.end('Home Page\n');
+	}else if(pathname === '/about'){
+		res.writeHead(200, {'Content-Type':'text/plain'});
+		res.end('About Us\n');		
+	}else if(pathname === '/redirect'){
+		res.writeHead(301, {'Location':'/'});
+		res.end('About Us\n');		
+	}else{
+		res.writeHead(404, {'Content-Type':'text/plain'});
+		res.end('Page Not Found!\n');		
+	}
 });
 
 s.listen(3000);
